13df16463f2b8abefddb3369c59a3af829ee471f
Sridhar Ratnakumar <srid@srid.ca>
2023-07-17 21:56:00 -0400

    Remove heist override (redundant)

--- flake.nix
+++ flake.nix
@@ -6,9 +6,6 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-root.url = "github:srid/flake-root";
     systems.url = "github:nix-systems/default";
-
-    heist.url = "github:snapframework/heist"; # Waiting for 1.1.1.0 on nixpkgs cabal hashes
-    heist.flake = false;
   };
 
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
@@ -22,10 +19,8 @@
       perSystem = { self', config, pkgs, ... }: {
         haskellProjects.default = {
           packages = {
-            heist.source = inputs.heist;
             pandoc-types.source = "1.23";
           };
-          settings.heist.check = false; # Tests are broken
           autoWire = [ "packages" "checks" ];
         };
 
