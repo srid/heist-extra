fdd4616ff8ec8a8bd2d934347d25b2ffbd6ad982
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-03-15 19:38:24 -0400

    Update Render.hs

--- src/Heist/Extra/Splices/Pandoc/Render.hs
+++ src/Heist/Extra/Splices/Pandoc/Render.hs
@@ -291,6 +291,6 @@ plainify = W.query $ \case
   B.Span _ _ -> ""
   -- TODO: How to wrap math stuff here?
   B.Math _mathTyp s -> s
-  -- Ignore footnoteor endnote
-  B.Note{} -> ""
+  -- Ignore footnote or endnote
+  B.Note {} -> ""
   _ -> ""
