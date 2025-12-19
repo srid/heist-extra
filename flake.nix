10a1958eb76e2648375f0cf9d3385fb8627aa7fd
Sridhar Ratnakumar <srid@srid.ca>
2025-12-18 19:14:32 -0500

    Fuck treefmt for now

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-root.url = "github:srid/flake-root";
     systems.url = "github:nix-systems/default";
   };
@@ -14,7 +13,6 @@
       imports = [
         inputs.haskell-flake.flakeModule
         inputs.flake-root.flakeModule
-        inputs.treefmt-nix.flakeModule
       ];
       perSystem = { self', config, pkgs, ... }: {
         haskellProjects.default = {
@@ -24,28 +22,9 @@
           autoWire = [ "packages" "checks" ];
         };
 
-        treefmt.config = {
-          inherit (config.flake-root) projectRootFile;
-          package = pkgs.treefmt;
-
-          programs.ormolu.enable = true;
-          programs.nixpkgs-fmt.enable = true;
-          programs.cabal-fmt.enable = true;
-
-          # We use fourmolu
-          programs.ormolu.package = pkgs.haskellPackages.fourmolu;
-          settings.formatter.ormolu = {
-            options = [
-              "--ghc-opt"
-              "-XImportQualifiedPost"
-            ];
-          };
-        };
-
         devShells.default = pkgs.mkShell {
           inputsFrom = [
             config.haskellProjects.default.outputs.devShell
-            config.treefmt.build.devShell
           ];
         };
 
